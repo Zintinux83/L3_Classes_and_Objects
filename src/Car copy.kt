509a0279a3f5244fd963828d7cc81c00302fc3b0
Zintinux83 <rebinsor1300estudio@gmail.com>
2024-11-03 13:20:54 +0100

    V1103

--- src/Car copy.kt
+++
@@ -1,9 +0,0 @@
-class Car (val car_model: String) {
-    var maxSpeed: Int = 0
-    constructor(model: String, maxSpeed: Int): this(model){
-        this.maxSpeed = maxSpeed
-    }
-    fun showInfo(){
-        println("Car: Model = $car_model, Max Speed = $maxSpeed")
-    }
-}
